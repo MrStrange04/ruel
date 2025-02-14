3020a0cd8bd1ea913689cab49419bff9f7c37ae5
rukawawa <160824779+MrStrange04@users.noreply.github.com>
2025-02-14 14:04:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
 onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
+  
+      const titles = ("Happy Valentine's Day, I Love You!").split('')
+      const titleElement = document.getElementById('title');
+      let index = 0;
+  
+      function appendTitle() {
+        if (index < titles.length) {
+          titleElement.innerHTML += titles[index];
+          index++;
+          setTimeout(appendTitle, 150); // 1000ms delay
+        }
+      }
+  
+      appendTitle();
+  
       clearTimeout(c);
     }, 1000);
   };
